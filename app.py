f5663bb1852021682fa27c870da7fd2be9d11ddf
Schmitzii <63397150+Schmitzii@users.noreply.github.com>
2021-12-18 18:23:36 +0100

    Initial commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import constants as c
 import helpers as h
 import tkinter as tk
+
 # messagebox does not get imported automatically, so import it explicitly
 from tkinter import messagebox
 
@@ -9,7 +10,7 @@ from tkinter import messagebox
 add_charge = False
 remove_charge = False
 
-# apply method to create icrcle to tk.Canvas
+# apply method to create circle to tk.Canvas
 tk.Canvas.create_circle = h._create_circle
 
 
@@ -17,7 +18,7 @@ def onClick_addCharge():
     global add_charge, remove_charge
     remove_charge = False
 
-    if(add_charge):
+    if add_charge:
         add_charge = False
         addcharge_button.config(relief=tk.RAISED)
     else:
@@ -30,7 +31,7 @@ def onClick_removeCharge():
     global add_charge, remove_charge
     add_charge = False
 
-    if(remove_charge):
+    if remove_charge:
         remove_charge = False
         removecharge_button.config(relief=tk.RAISED)
     else:
@@ -44,14 +45,26 @@ def onClick_inputField(event):
     if add_charge:
         if len(h.charges) > 9:
             messagebox.showerror(
-                title="Zu viele Ladungen", message="Es können maximal 10 Ladungen eingefügt werden.")
+                title="Zu viele Ladungen",
+                message="Es können maximal 10 Ladungen eingefügt werden.",
+            )
         else:
             q = canvas.create_circle(x, y, c.DEFAULT_CIRCLE_RADIUS, fill="red")
             coords = canvas.coords(q)
             # add new charge to charges array by calculating center coordinates of circle
             h.charges.append(
-                h.charge(1, [(coords[0] + coords[2]) / 2, (coords[1] + coords[3]) / 2]))
+                h.Charge(
+                    1, [(coords[0] + coords[2]) / 2, (coords[1] + coords[3]) / 2], q
+                )
+            )
             print(h.charges[-1].__dict__)  # print out object attributes
+    elif not add_charge and not remove_charge:
+        for q in h.charges:  # loop through to charges to check if item was clicked
+            coords = canvas.coords(q[2])
+            if (
+                coords[0] < x < coords[2] and coords[1] < y < coords[3]
+            ):  # check if mouse click was inside the item
+                pass
 
 
 """HIER WEITER MACHEN(KREIS HINZUFÜGEN UND GRÖßEN-TOGGLER)"""
@@ -59,7 +72,7 @@ def onClick_inputField(event):
 
 """ROOT"""
 root = tk.Tk()  # initialise root element
-root.title('Visualisierung elektrischer Feldlinien')  # set title
+root.title("Visualisierung elektrischer Feldlinien")  # set title
 root.geometry("1200x800")  # set window size
 
 """MENU"""
@@ -80,7 +93,8 @@ root.config(menu=menubar, bg=c.BACKGROUND_COLOR)
 
 """WINDOW"""
 window_f = tk.LabelFrame(
-    root, text="Visualisierung elektrischer Feldlinien", padx=10, pady=10)
+    root, text="Visualisierung elektrischer Feldlinien", padx=10, pady=10
+)
 window_f.pack(padx=10, pady=10)  # add padding to inside of frame
 
 """PLOT"""
@@ -90,11 +104,15 @@ buttons_f = tk.Frame(plot_f)
 # Buttons
 cursor_button = tk.Button(buttons_f, text="Cursor", width=c.BUTTON_WIDTH)
 addcharge_button = tk.Button(
-    buttons_f, text="Ladung hinzufügen", width=c.BUTTON_WIDTH, command=onClick_addCharge)
+    buttons_f, text="Ladung hinzufügen", width=c.BUTTON_WIDTH, command=onClick_addCharge
+)
 removecharge_button = tk.Button(
-    buttons_f, text="Ladung entfernen", width=c.BUTTON_WIDTH, command=onClick_removeCharge)
-efield_button = tk.Button(
-    buttons_f, text="Elektrisches Feld", width=c.BUTTON_WIDTH)
+    buttons_f,
+    text="Ladung entfernen",
+    width=c.BUTTON_WIDTH,
+    command=onClick_removeCharge,
+)
+efield_button = tk.Button(buttons_f, text="Elektrisches Feld", width=c.BUTTON_WIDTH)
 cursor_button.grid(row=0, column=0)
 addcharge_button.grid(row=0, column=1)
 removecharge_button.grid(row=0, column=2)
@@ -102,9 +120,8 @@ efield_button.grid(row=0, column=3)
 
 # Input Field
 canvas = tk.Canvas(plot_f, width=c.PLOT_SIZE, height=c.PLOT_SIZE)
-input_field = canvas.create_rectangle(
-    0, 0, c.PLOT_SIZE, c.PLOT_SIZE, fill='white')
-canvas.tag_bind(input_field, '<Button-1>', onClick_inputField)
+input_field = canvas.create_rectangle(0, 0, c.PLOT_SIZE, c.PLOT_SIZE, fill="white")
+canvas.tag_bind(input_field, "<Button-1>", onClick_inputField)
 
 buttons_f.grid(column=0, row=0)
 canvas.grid(column=0, row=1)
